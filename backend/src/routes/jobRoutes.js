fe9fb018fae5d2f259a4ce3bec3b2c90a8fa7e64
HALIA RUTAB (heyitslia) <haliarutab24@gmail.com>
2025-11-01 15:19:45 +0500

    Delete backend directory

--- backend/src/routes/jobRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import getAllJobs from "../controllers/jobController.js";
-
-const router = express.Router();
-
-router.get("/all-jobs", getAllJobs);
-
-export default router;
